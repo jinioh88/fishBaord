821e85b4820313b2b5c59ea14b12a1c977cd20bc
오세진 <jinioh88@gmail.com>
2018-07-11 21:40:36 +0900

    환경설정

---
+++ src/main/java/com/fishing/fishboard/FishboardApplication.java
@@ -0,0 +1,13 @@
+package com.fishing.fishboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishboardApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(FishboardApplication.class, args);
+    }
+}
---
+++ src/test/java/com/fishing/fishboard/FishboardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fishing.fishboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FishboardApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
