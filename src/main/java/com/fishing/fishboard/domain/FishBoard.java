51716339810abee116171768ba378d60d75f7a4b
오세진 <jinioh88@gmail.com>
2018-07-18 16:05:00 +0900

    fishboard 도메인

---
+++ src/main/java/com/fishing/fishboard/domain/FishBoard.java
@@ -0,0 +1,32 @@
+package com.fishing.fishboard.domain;
+
+import com.sun.tools.javac.comp.Todo;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@Entity
+@Table(name="tbl_fishboards")
+@EqualsAndHashCode(of = "bno")
+@ToString
+public class FishBoard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+    private String title;
+    private String writer;
+    private String content;
+    // Todo 지역, 낚시터명, 사진 추가 되야
+    @CreationTimestamp
+    private Timestamp regdate;
+    @UpdateTimestamp
+    private Timestamp updatedate;
+}
