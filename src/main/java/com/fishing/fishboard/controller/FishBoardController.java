600d119218817fda9d0177f2a7d62b76e475a9e4
오세진 <jinioh88@gmail.com>
2018-07-19 12:19:22 +0900

    화면 출력 페이징

--- src/main/java/com/fishing/fishboard/controller/FishBoardController.java
+++ src/main/java/com/fishing/fishboard/controller/FishBoardController.java
@@ -1,10 +1,14 @@
 package com.fishing.fishboard.controller;
 
+import com.fishing.fishboard.domain.FishBoard;
+import com.fishing.fishboard.persistence.FishBoardRepository;
+import com.fishing.fishboard.vo.PageVO;
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,10 +16,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/boards/")
 @Log
 public class FishBoardController {
-    @GetMapping("/list")
-    public void list(@PageableDefault(direction = Sort.Direction.DESC,
-    sort = "bno", size = 10, page = 0)Pageable page) {
-        log.info("list()..."+page);
-    }
 
+    @Autowired
+    private FishBoardRepository repository;
+
+    // 보안에 문제가 있음
+    //    @GetMapping("/list")
+//    public void list(@PageableDefault(direction = Sort.Direction.DESC,
+//    sort = "bno", size = 10, page = 0)Pageable page) {
+//        log.info("list()..."+page);
+//    }
+
+    @GetMapping("/list")
+    public void list(PageVO vo, Model model) {
+        Pageable page = vo.makePageable(0,"bno");
+        Page<FishBoard> result = repository.findAll(repository.makePredicate(null,null),page);
+        log.info(""+page);
+        log.info(""+result);
+
+        model.addAttribute("result",result);
+    }
 }
