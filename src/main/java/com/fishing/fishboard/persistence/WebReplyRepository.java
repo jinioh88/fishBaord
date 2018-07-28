95693a1cf7af46482bc540052bdc9741093cbcfe
오세진 <jinioh88@gmail.com>
2018-07-28 15:26:32 +0900

    댓글처리 js

--- src/main/java/com/fishing/fishboard/persistence/WebReplyRepository.java
+++ src/main/java/com/fishing/fishboard/persistence/WebReplyRepository.java
@@ -1,7 +1,14 @@
 package com.fishing.fishboard.persistence;
 
+import com.fishing.fishboard.domain.FishBoard;
 import com.fishing.fishboard.domain.WebReply;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface WebReplyRepository extends CrudRepository<WebReply,Long> {
+    @Query("select r from WebReply r where r.board = ?1 and r.rno > 0 order by r.rno asc")
+    public List<WebReply> getWebRepliesOfBoard(FishBoard board);
+
 }
