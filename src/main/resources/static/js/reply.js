43747b3c66e97ed3ec1d3542c95837ffa8daa7b5
오세진 <jinioh88@gmail.com>
2018-07-30 12:10:50 +0900

    댓글 수정

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -16,9 +16,18 @@ var replyManager = (function () {
        });
    };
 
-    var update = function (obj,callback) {
-        console.log("update...");
-    };
+   var update = function (obj, callback) {
+     console.log("update...");
+
+     $.ajax({
+        type: 'put',
+        url: '/replies/'+obj.bno,
+        dataType: 'json',
+        data: JSON.stringify(obj),
+         contentType: "application/json",
+         success:callback
+     });
+   };
 
     var add = function (obj, callback) {
         console.log("add...");
