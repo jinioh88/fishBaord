56d6e58afa948fc620f1db6595b7b2d61e15e165
오세진 <jinioh88@gmail.com>
2018-08-11 11:19:36 +0900

    rememberme token

--- src/main/java/com/fishing/fishboard/security/SecurityConfig.java
+++ src/main/java/com/fishing/fishboard/security/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 import javax.sql.DataSource;
 
@@ -14,6 +16,16 @@ import javax.sql.DataSource;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     FishUsersService fishUsersService;
+
+    @Autowired
+    DataSource dataSource;
+
+    private PersistentTokenRepository getJDBCRepository() {
+        JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
+        repo.setDataSource(dataSource);
+        return repo;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -24,8 +36,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin().loginPage("/login");
         http.exceptionHandling().accessDeniedPage("/accessDenied");
         http.logout().logoutUrl("/logout").invalidateHttpSession(true);
-        http.userDetailsService(fishUsersService);
+        http.rememberMe().key("fish")
+                .userDetailsService(fishUsersService)
+                .tokenRepository(getJDBCRepository())
+                .tokenValiditySeconds(60*60*24);
     }
-
 
 }
