a27268ad86fe2636e5106d58a17ab369fa5fe6dc
오세진 <jinioh88@gmail.com>
2018-09-04 13:18:20 +0900

    조행기 컨트롤러

--- src/main/java/com/fishing/fishboard/controller/JohangController.java
+++ src/main/java/com/fishing/fishboard/controller/JohangController.java
@@ -1,11 +1,17 @@
 package com.fishing.fishboard.controller;
 
+import com.fishing.fishboard.domain.JohangBoard;
 import com.fishing.fishboard.persistence.JohangRepository;
 import groovy.util.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/johang")
@@ -15,7 +21,20 @@ public class JohangController {
     JohangRepository repository;
 
     @GetMapping
-    public String board() {
+    public String board(Model model) {
+        List<JohangBoard> list = repository.findAll();
+        for(int i=0;i<list.size();i++) {
+            System.out.println(list.get(i).getMember().getUname());
+        }
+        model.addAttribute("list",list);
+        return "/johang/johang";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        JohangBoard board = repository.findByJno(1L);
+        model.addAttribute("board",board.getContent());
+        System.out.println(board.getContent());
         return "/johang/johang";
     }
 }
